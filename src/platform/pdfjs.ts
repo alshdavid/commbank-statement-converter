09634e28bb76a1d8a5a1b91f41883ce121e6cdfa
David Alsh <alshdavid@gmail.com>
2023-10-27 16:07:56 +1100

    pipeline

--- src/platform/pdfjs.ts
+++ src/platform/pdfjs.ts
@@ -4,19 +4,23 @@ import { Env } from './env'
 
 let pdfjsLoaded: boolean = false
 let pdfjsResolve: (v: typeof pdfjs) => void
+
 const pdfjsLib = new Promise<typeof pdfjs>(res => {
   pdfjsResolve = res
 })
+
+let worker: Worker | null
 
 export function getPdfJsLib(): Promise<typeof pdfjs> {
   return pdfjsLib
 }
 
+// Loading this lazily because it's quite heavy
 export async function initPdfjs() {
   if (pdfjsLoaded) return
+  worker = new Worker(Env.baseHref + '/vendor/pdfjs.worker.min.js')
   await createScript(Env.baseHref + '/vendor/pdfjs.min.js')
-  await createScript(Env.baseHref + '/vendor/pdfjs.worker.min.js')
-  windowRef.pdfjsLib.GlobalWorkerOptions.workerSrc = Env.baseHref + '/vendor/pdfjs.worker.min.js'
+  windowRef.pdfjsLib.GlobalWorkerOptions.workerPort = worker
   pdfjsResolve(windowRef.pdfjsLib)
   pdfjsLoaded = true
 }
